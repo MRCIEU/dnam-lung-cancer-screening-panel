c9f029f827fabbe6ec1a7086abc990f41c83c424
Matthew Suderman <matthewsuderman@hotmail.com>
2023-06-14 15:50:24 +0100

    source folders

---
+++ compile-panel.r
@@ -0,0 +1,35 @@
+sources <- c(
+    "age",
+    "ancestry",
+    "blood-cell-types",
+    "bmi",
+    "breast-cancer",
+    "cadmium",
+    "colorectal-cancer",
+    "copd",
+    "cotinine",
+    "dunedin-pace",
+    "educational-attainment",
+    "episcores",
+    "lung-cancer",
+    "prostate-cancer",
+    "prostate-cancer-t-cells",
+    "smoking-cessation",
+    "smoking-current-vs-former",
+    "smoking-status")
+
+files <- file.path(sources, "regions.csv")
+
+cols <- c("chr","start","end","source","details")
+panel <- lapply(sources, function(src) {
+    filename <- file.path(src, "regions.csv")
+    cat("loading ", filename, "\n")
+    stopifnot(file.exists(filename))                
+    regions <- read.csv(filename)
+    regions$source <- src
+    stopifnot(cols %in% colnames(regions))
+    regions[,cols]
+})
+panel <- do.call(rbind, panel)
+
+write.csv(panel, file="panel.csv", row.names=F)
