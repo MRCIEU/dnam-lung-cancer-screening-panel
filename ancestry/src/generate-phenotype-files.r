0379f60e9c08d477aed978796d0af7babb12210d
Matthew Suderman <matthewsuderman@hotmail.com>
2023-06-20 11:48:28 +0100

    fixes phenotype file generation error

--- ancestry/src/generate-phenotype-files.r
+++ ancestry/src/generate-phenotype-files.r
@@ -6,14 +6,18 @@ dir.create(output.dir <- args[1])
 pheno.url <- "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/integrated_call_samples_v3.20130502.ALL.panel"
 
 library(data.table)
-pheno <- fread(pheno.url, select = 1:2)
-colnames(pheno) <- c("IID", "phenotype")
+pheno <- fread(pheno.url, select = 1:3)
+pheno <- as.data.frame(pheno)
 
-for (ancestry in unique(pheno$phenotype)) {
-  design <- pheno
-  design$phenotype <- sign(design$phenotype == ancestry)
-  write.table(
-    x=design, 
-    file=file.path(output.dir, paste0(ancestry, ".txt")),
-    quote = F, row.names = F, col.names = T)
+for (varname in c("pop","super_pop")) {
+    for (ancestry in unique(pheno[[varname]])) {
+        design <- pheno[,c("sample",varname)]
+        colnames(design) <- c("IID","phenotype")
+        design$phenotype <- sign(design$phenotype == ancestry)
+        write.table(
+            x=design, 
+            file=file.path(output.dir, paste0(ancestry, ".txt")),
+            quote = F, row.names = F, col.names = T)
+    }
 }
+
