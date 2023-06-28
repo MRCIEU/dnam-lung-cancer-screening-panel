75d02192f0eeabc0054bc9ab47415c0b58e68737
ryanlangdon <ryan.langdon@bristol.ac.uk>
2023-06-28 18:12:58 +0100

    mccartney hdl predictor supplement and filtering

---
+++ hdl/src/filter-mccartney-sites.R
@@ -0,0 +1,13 @@
+library(data.table)
+setwd("~/hdl-cholesterol")
+
+## Read in McCartney et al. (https://doi.org/10.1186/s13059-018-1514-1) alcohol
+## predictor CpGs and weights from Supplementary Table S3
+mccartney <- fread("mccartney_supplement_s6.csv")
+
+## Restrict sites to those with an effect of >=0.1
+mccartney_restricted <- mccartney[mccartney$Beta>=0.1,]
+
+## Change column names and write csv
+colnames(mccartney_restricted) <- c("cpg", "coefficient")
+write.csv(mccartney_restricted, file = "mccartney-sites.csv", quote = F, row.names = F)
