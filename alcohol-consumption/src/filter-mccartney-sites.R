5e5cfef7f6cf6169f7f3d74acd24d616377ad370
ryanlangdon <37299062+ryanlangdon@users.noreply.github.com>
2023-06-28 18:10:51 +0100

    Update filter-mccartney-sites.R

--- alcohol-consumption/src/filter-mccartney-sites.R
+++ alcohol-consumption/src/filter-mccartney-sites.R
@@ -3,7 +3,7 @@ setwd("~/alcohol-consumption")
 
 ## Read in McCartney et al. (https://doi.org/10.1186/s13059-018-1514-1) alcohol
 ## predictor CpGs and weights from Supplementary Table S3
-mccartney <- fread("../Downloads/mccartney_supplement_s3.csv")
+mccartney <- fread("mccartney_supplement_s3.csv")
 
 ## Restrict sites to those with an effect of >=0.1
 mccartney_restricted <- mccartney[mccartney$Beta>=0.1,]
